220fd0899884d53ac3cf428447bb41a73f398106
Wells Tsai <wells.z.tsai@gmail.com>
2018-08-28 01:32:07 -0500

    hit spot replenish implemented

--- src/client/views/game/new-game/setup-board/renderStartTiles.js
+++ src/client/views/game/new-game/setup-board/renderStartTiles.js
@@ -64,8 +64,9 @@ const isWithinBounds = (sprite, bounds) => {
 
 const positionHitSpots = (parentTile, storeHitSpots) => {
   const { id, x, y, width, height } = parentTile;
-  const relevantHitSpots = storeHitSpots.filter((hitSpot) => hitSpot.parentId === id);
 
+  const relevantHitSpots = storeHitSpots.filter((hitSpot) => hitSpot.parentId === id);
+console.log('relevanthitspots position', relevantHitSpots)
   // on initialize, has direction, no hitspots destroyed
   // on regular move, same as above
   // on snap, hitspot becomes destroyed, want to keep it that way
@@ -98,6 +99,29 @@ const positionHitSpots = (parentTile, storeHitSpots) => {
   });
 };
 
+const createHitSpot = (store, mainBoardTileSprite, direction) => {
+  const { mainBoardLayer } = getGameState(store);
+
+  mainBoardTileSprite[direction] = new PIXI.Sprite(PIXI.Texture.WHITE);
+  mainBoardTileSprite[direction].alpha = 1;
+  mainBoardTileSprite[direction].tint = 0x36c2ed;
+  mainBoardTileSprite[direction].width = mainBoardTileSprite.width;
+  mainBoardTileSprite[direction].height = mainBoardTileSprite.height;
+  mainBoardTileSprite[direction].interactive = true;
+  mainBoardTileSprite[direction].x = -mainBoardTileSprite.width;
+  mainBoardTileSprite[direction].id = shortid.generate();
+  mainBoardTileSprite[direction].parentId = mainBoardTileSprite.id;
+  mainBoardTileSprite[direction].covered = false;
+  mainBoardTileSprite[direction].direction = direction;
+
+  store.dispatch({
+    type: 'ADD_HIT_SPOT',
+    hitSpot: mainBoardTileSprite[direction],
+  });
+
+  mainBoardLayer.addChild(mainBoardTileSprite[direction]);
+};
+
 const autoSnapIfCollision = (parentTile, store) => {
   const { x, y } = parentTile;
   const { mainBoardTileGraph, hitSpots } = getGameState(store);
@@ -112,7 +136,6 @@ const autoSnapIfCollision = (parentTile, store) => {
     return acc;
   }, []);
 
-  console.log('possibleHitSpots', possibleHitSpots);
   // check which hitspot is closest to tile, add to an array relevantHitSpots to account for same dimensions
   // snap tile to closest
   let relevantHitSpots = [];
@@ -130,14 +153,10 @@ const autoSnapIfCollision = (parentTile, store) => {
     }
   });
 
-  console.log('relevantHitSpots', relevantHitSpots);
-
   if (relevantHitSpots.length) {
     // autosnap
     parentTile.x = relevantHitSpots[0].x + (relevantHitSpots[0].width / 2);
     parentTile.y = relevantHitSpots[0].y + (relevantHitSpots[0].height / 2);
-
-    console.log('mianboardtilegraph', mainBoardTileGraph);
 
     relevantHitSpots.forEach((hitSpot) => {
       const hitTile = mainBoardTileGraph[hitSpot.parentId];
@@ -148,15 +167,14 @@ const autoSnapIfCollision = (parentTile, store) => {
       hitTile[direction] = placedTile;
       placedTile[oppositeDirection[direction]] = hitTile;
       
-      console.log('***destroying')
       // remove hitspots
       hitSpot.destroy();
       const placedTileHitSpot = hitSpots.find((hitSpot) => (hitSpot.parentId === placedTile.id) && (hitSpot.direction === oppositeDirection[direction]));
       placedTileHitSpot.destroy();
     });
 
-    // // clean out destroyed hit spots
-    store.dispatch({ type: 'CLEAN_DESTROYED_HIT_SPOTS' });
+    // clean out destroyed hit spots
+    // store.dispatch({ type: 'CLEAN_DESTROYED_HIT_SPOTS' });
   } else {
     const directions = ['top', 'bottom', 'left', 'right'];
     const placedTile = mainBoardTileGraph[parentTile.id];
@@ -165,7 +183,6 @@ const autoSnapIfCollision = (parentTile, store) => {
         const previouslyHitTile = placedTile[direction];
         previouslyHitTile[oppositeDirection[direction]] = null;
         placedTile[direction] = null;
-        // re-add hitspot TODO *********************************
       }
     });
   }
@@ -193,24 +210,7 @@ function onPlayerTileDragStart(event, store) {
 
   const hitSpotTiles = ['top', 'bottom', 'left', 'right'];
   hitSpotTiles.forEach((hitSpot) => {
-    mainBoardTileSprite[hitSpot] = new PIXI.Sprite(PIXI.Texture.WHITE);
-    mainBoardTileSprite[hitSpot].alpha = 1; // change to 0
-    mainBoardTileSprite[hitSpot].tint = 0x36c2ed;
-    mainBoardTileSprite[hitSpot].width = mainBoardTileSprite.width;
-    mainBoardTileSprite[hitSpot].height = mainBoardTileSprite.height;
-    mainBoardTileSprite[hitSpot].interactive = true;
-    mainBoardTileSprite[hitSpot].x = -mainBoardTileSprite.width;
-    mainBoardTileSprite[hitSpot].id = shortid.generate();
-    mainBoardTileSprite[hitSpot].parentId = mainBoardTileSprite.id;
-    mainBoardTileSprite[hitSpot].covered = false;
-    mainBoardTileSprite[hitSpot].direction = hitSpot;
-
-    store.dispatch({
-      type: 'ADD_HIT_SPOT',
-      hitSpot: mainBoardTileSprite[hitSpot],
-    });
-
-    mainBoardLayer.addChild(mainBoardTileSprite[hitSpot]);
+    createHitSpot(store, mainBoardTileSprite, hitSpot);
   });
 
   mainBoardTileSprite
@@ -228,7 +228,7 @@ function onMainBoardTileSpritePointerMove(event, app) {
 }
 
 function onMainBoardTileSpritePointerDown(event, store, playerTileSprite) {
-  const { app, mainBoardBounds, hitSpots } = getGameState(store);
+  const { app, mainBoardBounds, hitSpots, mainBoardTileGraph } = getGameState(store);
   
   if (!isWithinBounds(this, mainBoardBounds) && !playerTileSprite._destroyed) {
     this.destroy();
@@ -239,6 +239,8 @@ function onMainBoardTileSpritePointerDown(event, store, playerTileSprite) {
 
     positionHitSpots(this, hitSpots);
   } else if (isWithinBounds(this, mainBoardBounds) && !playerTileSprite._destroyed) {
+    positionHitSpots(this, hitSpots);
+
     store.dispatch({
       type: 'DELETE_PLAYER_TILE',
       index: playerTileSprite.index,
@@ -250,6 +252,7 @@ function onMainBoardTileSpritePointerDown(event, store, playerTileSprite) {
       mainBoardTile: {
         id: this.id,
         letter: this.letter,
+        reference: this,
         up: null,
         down: null,
         left: null,
@@ -271,7 +274,6 @@ function onMainBoardTileSpritePointerDown(event, store, playerTileSprite) {
     
     if (!this._destroyed) {
       autoSnapIfCollision(this, store);
-
       positionHitSpots(this, hitSpots);
     }
   } else {
@@ -284,8 +286,41 @@ function onMainBoardTileSpritePointerDown(event, store, playerTileSprite) {
       isDragging: true,
     });
 
+    // replenish destroyed hitspots, including ones left
+    const associatedHitSpots = hitSpots.filter((hitSpot) => hitSpot.parentId === this.id);
+    const destroyedHitSpotDirections = [];
+    associatedHitSpots.forEach((hitSpot) => {
+      if (hitSpot._destroyed) {
+        destroyedHitSpotDirections.push(hitSpot.direction);
+      }
+    });
+
+    destroyedHitSpotDirections.forEach((direction) => {
+      createHitSpot(store, this, direction);
+    });
+
+    // find parentId of reciprocal hit spot parent, then search store hit spots to restore destroyed
+    destroyedHitSpotDirections.forEach((direction) => {
+      const reciprocalParentId = mainBoardTileGraph[this.id][direction].id;
+      const associatedHitSpots = hitSpots.filter((hitSpot) => hitSpot.parentId === reciprocalParentId);
+      const destroyedHitSpotDirections = [];
+      associatedHitSpots.forEach((hitSpot) => {
+        if (hitSpot._destroyed) {
+          destroyedHitSpotDirections.push(hitSpot.direction);
+        }
+      });
+
+      destroyedHitSpotDirections.forEach((direction) => {
+        const reciprocalTile = mainBoardTileGraph[reciprocalParentId].reference;
+        createHitSpot(store, reciprocalTile, direction);
+        positionHitSpots(reciprocalTile, getGameState(store).hitSpots);
+      });
+    });
+
+    store.dispatch({ type: 'CLEAN_DESTROYED_HIT_SPOTS' });
+    const updatedHitSpots = getGameState(store).hitSpots;
     // set hit spots alpha 1, and move own hit spots out of board
-    hitSpots.forEach((hitSpot) => {
+    updatedHitSpots.forEach((hitSpot) => {
       hitSpot.alpha = 1;
       if (hitSpot.parentId === this.id) {
         hitSpot.x = -hitSpot.width;
